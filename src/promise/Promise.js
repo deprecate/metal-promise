6a20493a73ec2848492953aee4e703dffd9e4ee6
Maira Bello <maira.araujo@liferay.com>
2016-01-27 10:24:34 -0300

    Removes "npm:" prefix

--- src/promise/Promise.js
+++ src/promise/Promise.js
@@ -10,8 +10,8 @@
 
 'use strict';
 
-import core from 'npm:metal/src/core';
-import async from 'npm:metal/src/async/async';
+import core from 'metal/src/core';
+import async from 'metal/src/async/async';
 
 /**
  * Provides a more strict interface for Thenables in terms of
